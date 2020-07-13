9879eee789b120732c6ebacd8ae861e03bc3056d
Quoctuan1780 <Quoctuan1780@gmail.com>
2020-07-13 23:16:37 +0700

    Sua doi layout hien thi full, an thanh dieu huong, di chuyen EditText khi hien ban phim

--- app/src/main/java/com/example/appmusicbotnav/activity/DangNhap.java
+++ app/src/main/java/com/example/appmusicbotnav/activity/DangNhap.java
@@ -157,4 +157,16 @@ public class DangNhap extends AppCompatActivity {
 
         return cm.getActiveNetworkInfo() != null && cm.getActiveNetworkInfo().isConnected();
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+    }
 }
